5cb3cb44c063b1e35f10374ec7277c276bd3c3ce
Mohammad Saif Sabir <mohammad.sabir@grabtaxi.com>
2021-11-06 00:15:11 +0530

    removed redundant function in main

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import draw
 from lan import LAN
 
 
-def process_network():
+if __name__ == "__main__":
     lan = LAN()
     port_scan_results = lan.port_scan_network()
     # draw a graph with the results
     draw.create_graph(lan.get_router_ip(), port_scan_results)
-
-
-if __name__ == "__main__":
-    process_network()
